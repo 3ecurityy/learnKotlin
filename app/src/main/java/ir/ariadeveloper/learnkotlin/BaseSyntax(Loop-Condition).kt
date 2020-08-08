2502944b0147b8aea1426e829788e3f98b7af968
MohammadReza <3ecurityy@gmail.com>
2020-08-08 19:56:00 +0430

    Break an Jump  - BaseSyntax(Loop-Condition).kt

--- app/src/main/java/ir/ariadeveloper/learnkotlin/BaseSyntax(Loop-Condition).kt
+++ app/src/main/java/ir/ariadeveloper/learnkotlin/BaseSyntax(Loop-Condition).kt
@@ -1,20 +1,24 @@
 package ir.ariadeveloper.learnkotlin
 
 fun loop() {
+    //Repeat
     repeat(3) {
         println("Repeat $it")
     }
 
+    //While
     var conditionWhile = 10
     while (conditionWhile <= 10) {
         println("this is While Loop")
         conditionWhile++
     }
 
+    //For
     for (i in 1 to 10) {
         println("this is for Loop")
     }
 
+    //When
     val conditionWhen = 5
     when (conditionWhen) {
         1 -> {
@@ -34,7 +38,21 @@ fun loop() {
         }
         else -> print("else")
     }
+
+    //Do While
     do {
         println("Do")
     } while (false)
+
+
+    //Break an Jump
+    loop@ for (i in 1 to 100) {
+        for (j in 1 to 100) {
+            var result = i * j
+            if (result > 500) {
+                break@loop
+            }
+            println(result)
+        }
+    }
 }
